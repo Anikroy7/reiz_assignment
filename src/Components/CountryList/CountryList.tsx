5214dd2ff161606f414698e423dbf81a8201f3d2
Anik Kumer Roy <96816764+Anikroy7@users.noreply.github.com>
2023-06-20 13:50:08 +0600

    fecth data successfully

--- src/Components/CountryList/CountryList.tsx
+++ src/Components/CountryList/CountryList.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+type Country = {
+  name: String;
+  region: String;
+  area: String;
+};
 
 const CountryList: React.FC = () => {
+  const [countries, setCountries] = useState<Country[]>([]);
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch(
+        "https://restcountries.com/v2/all?fields=name,region,area"
+      );
+      const data = await response.json();
+      console.log(data);
+      setCountries(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <div>All CountryList</div>;
 };
 
